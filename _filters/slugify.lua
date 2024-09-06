ba94a9a80068d4a317bb4209e33488a77d34bba9
Patrick J. Roddy <patrickjamesroddy@gmail.com>
2024-09-06 10:11:21 +0100

    Add `lua` filter

---
+++ _filters/slugify.lua
@@ -0,0 +1,10 @@
+local function slugify(s)
+	return s:gsub("%s+", "-"):gsub("[^%w-]+", ""):lower()
+end
+
+return {
+	Meta = function(m)
+		m.slug = m.title and slugify(pandoc.utils.stringify(m.title))
+		return m
+	end,
+}
